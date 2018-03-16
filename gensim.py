51b75b009731660dc9d6cf1790f2058b9accf19f
maybefeicun <1575113137@qq.com>
2018-03-16 22:06:22 +0800

    gensim

---
+++ gensim.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/16 21:18
+# @Author  : chen
+# @File    : gensim.py
+# @Software: PyCharm
+
+# from gensim.models import Word2Vec
+# model = Word2Vec('word', sg=1, size=128,  window=5,  min_count=5,  negative=3, sample=0.001, hs=1, workers=4)
+
+
+dict= {'k':3, 'm':4}
+print(len(dict))
